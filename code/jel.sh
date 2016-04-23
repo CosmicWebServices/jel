96a91b3af46c1a9789ec637c02c15bb7f1cc27a1
Baran Skistad <bskist01@isd77.k12.mn.us>
2016-04-23 15:46:56 +0000

    Added shell script

---
+++ code/jel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+
+echo "  ______                     ___  "
+echo "  \     \                   /   \ "
+echo "   \_____\                  |   | "
+echo "     _____                  |   | "
+echo "     \    \                 |   | "
+echo "      \    \    __________  |   | "
+echo "       \    \  /   ____   \ |   | "
+echo "        \    \!   /____\  ! |   | "
+echo " _______/    / \    ______/ |   | "
+echo " \          /   \   \_____  |   | "
+echo "  \________/     \_______/  \___/ "
+echo
+echo "You're running with $USER"
