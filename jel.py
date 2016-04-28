9b5e02ce74ec5db41f70517d05c78445fcc90d6a
Baran Skistad <bskist01@isd77.k12.mn.us>
2016-04-28 12:20:18 +0000

    source Updated

--- jel.py
+++ jel.py
@@ -5,14 +5,13 @@ import sys
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='')
 validFile = False
 arg = sys.argv[1]
 version = '1.0.1'
 
 if arg[-4:] == '.jel':
     validFile = True
-    print 'File Vaildated'
+    print 'File Extension Validated'
 elif arg == '-help':
     print 'The following commands are all run with jel'
     print ''
@@ -24,8 +23,6 @@ elif arg == '-help':
 elif arg == '-version':
     print version
     sys.exit()
-elif arg == '-update':
-    subprocess.call('npm', 'install', '-g', 'jel')
 if not validFile:
     sys.exit('Invaild File')
 
@@ -74,31 +71,3 @@ def returnCommand(c):
             index = index + 1
         command = command[:-1]
         return command
-
-def returnLibrary(c):
-    num = 0
-    builtinNum = None
-    while num <= len(builtins):
-        if builtins[num] in c:
-            builtinNum = num
-        else:
-            num = num + 1
-    builtinsFound = False
-    if builtinsFound:
-        index = 0
-        current = ''
-        dotFound = False
-        commandFound = False
-        command = ''
-        Adone = False
-        while not Adone:
-            if index > len(c):
-                Adone = True
-            elif c[index - 1] == '.':
-                dotFound = True
-                current = current
-            elif dotFound:
-                command = command + c[index - 1]
-            index = index + 1
-        command = command[:-1]
-        return current
