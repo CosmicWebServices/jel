f900bf1e120050533cff658fefb1b651c75edb10
Baran Skistad <bskist01@isd77.k12.mn.us>
2016-04-30 14:27:57 +0000

    Added colors

--- jel.py
+++ jel.py
@@ -11,7 +11,7 @@ version = '1.0.3'
 
 if arg[-4:] == '.jel':
     validFile = True
-    print 'jel: file extension validated'
+    print 'jel: \033[92mfile extension validated\033[0m'
 elif arg == '-help':
     print 'The following commands are all run with jel'
     print ''
@@ -23,7 +23,7 @@ elif arg == '-version':
     print version
     sys.exit()
 if not validFile:
-    sys.exit('Invaild File')
+    sys.exit('jel: \033[91minvalid file\033[0m')
 
 
 done = False
@@ -34,11 +34,11 @@ builtins = ['terminal', 'file']
 with open(arg, 'r') as myfile:
     content = myfile.read().replace('\n', '')
 
-print 'jel: retrieved file contents'
+print 'jel: \033[92mretrieved file contents\033[0m'
 
 contents = [part + ';' for part in content[:-1].split(';')]
 
-print 'jel: file splitted'
+print 'jel: \033[92mfile splitted\033[0m'
 
 line = 0
 command = ''
